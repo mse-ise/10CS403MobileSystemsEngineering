2d3c8a887aa0f3e7355361ff7d74b06c085c7d06
Arjun <arjunmadan1992@gmail.com>
2013-08-31 19:28:04 +0530

    first commit

---
+++ Calculator.m
@@ -0,0 +1,40 @@
+#import "Calculator.h"
+
+@implementation Calculator:NSObject
+
+-(void)setNum1:(double)num
+{
+	num1 = num;
+}
+
+-(void)setNum2:(double)num
+{
+	num2 = num;
+}
+
+-(double)add
+{
+	return (num1+num2);
+}
+
+-(double)subtract
+{
+	return (num1-num2);
+}
+
+-(double)multiply
+{
+	return (num1*num2);
+}
+
+-(double)divide
+{
+	if(num2==0)
+	{
+		NSLog(@"Divide by 0 error");
+		return 0.0;
+	}
+	return (num1/num2);
+}
+
+@end
